9894d523e006aa3ab3ded6bb570875c1d1068d55
Amin <amin1382422amin@gmail.com>
2023-02-13 11:51:19 +0330

    1st session

---
+++ LeanerSerch/LeanerSearch.cs
@@ -0,0 +1,13 @@
+﻿int[] a = { 5, 2, 4, 6, 1, 3 };
+
+var b = Contain(a, 6);
+
+bool Contain(int[] array, int target)
+{
+	for (int i = 0; i < a.Length; i++)
+	{
+		if (array[i] == target)
+			return true;
+	}
+	return false;
+}
